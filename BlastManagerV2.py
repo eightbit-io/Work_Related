c97d3579066fe433858e18851e9e91e241a49996
Chris Saunders <chris.saunders@yancoal.com.au>
2016-12-30 11:05:20 +1000

    Created new cleaner version

---
+++ BlastManagerV2.py
@@ -0,0 +1,35 @@
+
+"""Clean skin blast manager"""
+
+class Drillhole:
+    """Generic Drill Hole Class"""
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "not yet implemented"
+
+class Blast:
+    """Generic Blast Class"""
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "not yet implemented"
+
+    def createdummyhole(self):
+        """create dummy holes for testing."""
+        pass
+
+    def createhole(self):
+        """Create individual holes"""
+        pass
+
+    def importcsv(self):
+        """import csv of holes"""
+        pass
+
+
+if __name__ == "__main__":
+    print("\nworking\n")
+
